1ed14f5fa24a6e298d61dc9fe6e1edc4b27dc80e
ThnnathatChaiphutha <thnnathatchaiphutha@gmail.com>
2025-03-11 11:13:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Define a route for the root URL ("/")
 @app.get("/")
 def read_root():
-    return {"message": "Hello, Thnnathat!"}
+    return {"message": "Hello, Bro!"}
 
 @app.get("/new")
 def new_endpoint():
